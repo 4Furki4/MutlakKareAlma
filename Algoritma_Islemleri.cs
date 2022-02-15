5fb9b0ecd8bf4dba56612d90b501a5f6c06c45b3
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-15 21:16:51 +0300

    Algoritmayı kuran ve elde edilen sonuçları double değişken alan listeye atayıp mezkur listeyi  döndüren AlgoritmaKur metodu tamamlandı

--- Algoritma_Islemleri.cs
+++ Algoritma_Islemleri.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Collections.Generic;
 namespace MutlakDeger
 {
     public class Algoritma_Islemleri
     {
-        
+        public List<double> AlgoritmaKur(List<int> list)
+        {
+            double kucukSayiFarklari=0;
+            double buyukSayiFarklari=0;
+            List<int> Kucukler= new List<int>();
+            List<int> Buyukler= new List<int>();
+            List<Double> Sonuc= new List<double>();
+            foreach (var sayi in list)
+            {
+                if (sayi<67)
+                {
+                    Kucukler.Add(sayi);
+                }else
+                {
+                    Buyukler.Add(sayi);
+                }
+            }
+            foreach (var item in Kucukler)
+            {
+                kucukSayiFarklari+=67-item;
+            }
+            foreach (var item in Buyukler)
+            {
+                buyukSayiFarklari=Math.Pow((Math.Abs(67-item)),2);
+            }
+            Sonuc.Add(kucukSayiFarklari);
+            Sonuc.Add(buyukSayiFarklari);
+            return Sonuc;
+        }
     }
 }
