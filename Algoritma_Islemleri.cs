6cc548de555f478d4f45490952c07191d4b89f7c
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-15 21:37:46 +0300

    Algoritma_Islemleri sınıfındaki if-else ve atama işlemleri daha kısa bir biçimde yazıldı.

--- Algoritma_Islemleri.cs
+++ Algoritma_Islemleri.cs
@@ -6,22 +6,12 @@ namespace MutlakDeger
     {
         public List<double> AlgoritmaKur(List<int> list)
         {
-            double kucukSayiFarklari=0;
-            double buyukSayiFarklari=0;
+            double kucukSayiFarklari=0, buyukSayiFarklari=0;
             List<Double> Sonuc= new List<double>();
             foreach (var item in list)
             {
-                if (item<67) //listeden gelen her sayının 67'den büyük olup olmamasına bakıyoruz ve küçükse
-                {
-                    kucukSayiFarklari+=67-item; // bu işlemi,
-                }
-                else
-                {
-                    buyukSayiFarklari+=Math.Pow((Math.Abs(67-item)),2); // büyükse bu işlemi gerçekleştiriyoruz.
-                }
-                
+                if(item<67){kucukSayiFarklari+=67-item;}else{buyukSayiFarklari+=Math.Pow((Math.Abs(67-item)),2);}
             }
-            
             Sonuc.Add(kucukSayiFarklari); // elde edilen toplam sayılarını listeye ekledim.
             Sonuc.Add(buyukSayiFarklari);
             return Sonuc; //Listeyi SonuclariYazdir metodunda kullanmak üzere döndürdüm.
